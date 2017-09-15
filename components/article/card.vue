d40e0a86828c82582da45ed8a5bb62136aead8f6
littlewin <littlewin.wang@sky-cloud.net>
2017-09-15 14:02:54 +0800

    feature: update article card

--- components/article/card.vue
+++ components/article/card.vue
@@ -2,6 +2,11 @@
   <div class="article-card">
     <div class="article-thumb">
       <img src="http://7xpot0.com1.z0.glb.clouddn.com/16-11-7/16133495.jpg" alt="">
+      <div class="background">
+        <span class="read-more">
+          文章详情
+        </span>
+      </div>
     </div>
     <div class="article-info">
       <div class="article-meta">
@@ -17,9 +22,9 @@
       </div>
       <div class="article-meta">
         <i class="iconfont icon-time">
-        </i>{{new Date().toLocaleDateString()}}&nbsp
+        </i>{{new Date().toLocaleDateString()}}&nbsp&nbsp&nbsp&nbsp
         <i class="iconfont icon-love">
-        </i>9&nbsp
+        </i>9&nbsp&nbsp&nbsp&nbsp
         <i class="iconfont icon-comment">
         </i>4
       </div>
@@ -38,6 +43,51 @@ export default {
   width: 240px;
   position: relative;
   border-radius: 5px;
+  .article-thumb {
+    position: relative;
+    .background {
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      z-index: 1;
+      background: rgba(0, 0, 0, 0.5);
+      color: white;
+      opacity: 0;
+      .read-more {
+        display: block;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        &:before {
+          content: '';
+          display: block;
+          width: 2px;
+          height: 1px;
+          position: absolute;
+          top: -10px;
+          left: 50%;
+          transform: translateX(-50%);
+          background: white;
+          transition: all 0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000);
+        }
+        &:after {
+          content: '';
+          display: block;
+          width: 2px;
+          height: 1px;
+          position: absolute;
+          bottom: -10px;
+          left: 50%;
+          transform: translateX(-50%);
+          background: white;
+          transition: all 0.5s cubic-bezier(0.075, 0.820, 0.165, 1.000);
+        }
+      }
+    }
+  }
   .article-info {
     .article-meta {
       margin: 10px 0;
@@ -59,6 +109,21 @@ export default {
       font-size: 14px;
       font-weight: 300;
       color: #777;
+    }
+  }
+  &:hover {
+    .article-thumb {
+      .background {
+        opacity: 1;
+        .read-more {
+          &:before {
+            width: 150%
+          }
+          &:after {
+            width: 150%
+          }
+        }
+      }
     }
   }
 }
