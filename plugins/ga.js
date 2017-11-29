c9dae09104a43af1685e76180de7f836e9208573
littlewin <littlewin.wang@sky-cloud.net>
2017-11-29 13:28:44 +0800

    feature: 将google analytics分解异步推送

--- plugins/ga.js
+++ plugins/ga.js
@@ -3,26 +3,53 @@
  * @author littlewin(littlewin.wang@gmail.com)
  */
 
+import Service from '~/plugins/axios'
+import Cookies from 'js-cookie'
+const uuidv4 = require('uuid/v4')
+
 /* eslint-disable */
 // 只在生产模式加载
-if (process.env.NODE_ENV === 'production') {
-  // Google 统计分析脚本
-  (function(i, s, o, g, r, a, m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)}, i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+// if (process.env.NODE_ENV === 'production') {
+if (1) {
+  // Generate Client information
+  let clientInfo = (window, document, navigator, location, route) => {
+    let screen = window.screen;
+    let encode = encodeURIComponent;
+    let cid = Cookies.get('_cid')
 
-  // 当前页的访问统计
-  ga('create', 'UA-108992108-1', 'auto')
-  ga('send', 'pageview')
+    if (!cid) {
+      cid = uuidv4()
+      Cookies.set('_cid', cid)
+    }
+
+    let data = [
+      'v=1',
+      't=pageview',
+      'cid=' + cid,
+      'tid=' + 'UA-108992108-1',
+      'dt=' + encode(route.fullPath),
+      'dr=' + encode(document.referrer),
+      'dp=' + encode(route.fullPath),
+      'ul=' + (navigator.language || navigator.browserLanguage),
+      'sd=' + screen.colorDepth + '-bit',
+      'sr=' + screen.width + 'x' + screen.height,
+      'z=' + (+new Date)
+    ]
+
+    return '?' + data.join('&')
+  }
 
   // 应用挂载后
   window.onNuxtReady((app) => {
+    Service.post('/ga', {
+      query: clientInfo(window, document, navigator, location, app.$route)
+    })
     // 页面路由切换
     app.$nuxt.$on('routeChanged', (to, from) => {
-      // 告诉 Google 统计分析服务 增加新的页面访问统计
-      ga('set', 'page', to.fullPath)
-      ga('send', 'pageview')
+      // 告诉后端统计分析服务 增加新的页面访问统计
+      Service.post('/ga', {
+        query: clientInfo(window, document, navigator, location, to)
+      })
     })
   })
 }
